8ed1cda7fb8fc2f6ad76c5121c49b2918f72ee9a
Sahar HAMMAMI <hammamisahar456@gmail.com>
2024-06-28 03:00:10 +0200

    g

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController');
+const productController = require('../controllers/produitsController');
 
-router.post("/api/add_produits", productController.addProduct);
-router.get("/api/get_produits", productController.getProducts);
-router.get("/api/get_produits_byName/:_Nom", productController.getProductsByName);
-router.get("/api/get_produits_byCategorie/:_Categorie", productController.getProductsByCategorie);
-router.get("/api/get_produits_byType/:_Type", productController.getProductsByType);
-router.put("/api/updateproduits/:id", productController.updateProduct);
-router.delete("/api/deletproduits/:id", productController.deleteProduct);
+router.post("/api/add_produits", productController.add_produits);
+router.get("/api/get_produits", productController.get_produits);
+router.get("/api/get_produits_byName/:_Nom", productController.get_produits_byName);
+router.get("/api/get_produits_byCategorie/:_Categorie", productController.get_produits_byCategorie);
+router.get("/api/get_produits_byType/:_Type", productController.get_produits_byType);
+router.put("/api/updateproduits/:id", productController.updateproduits);
+router.delete("/api/deletproduits/:id", productController.deletproduits);
 
 module.exports = router;
